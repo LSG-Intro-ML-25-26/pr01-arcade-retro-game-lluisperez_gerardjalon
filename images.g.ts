224ee4e7ff1fc6a6530cea8ebc1e0105a5767176
Gerard Jalón <gerard.jalon@gmail.com>
2026-01-14 11:31:54 +0100

    feat: add ice enemies sprites

--- images.g.ts
+++ images.g.ts
@@ -3,6 +3,25 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "bullet-1":return img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . 4 4 4 . . . . . . 
+. . . . . 4 5 5 5 4 . . . . . 
+. . . . . 4 5 5 5 4 . . . . . 
+. . . . . 4 5 5 5 4 . . . . . 
+. . . . . . 4 4 4 . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
             case "image1":
             case "nena-front":return img`
 . f f f . f f f f . f f f . 
@@ -21,6 +40,67 @@ f f f c 3 c c c c 3 c f f f
 . 4 4 f 6 6 6 6 6 6 f 4 4 . 
 . . . . f f f f f f . . . . 
 . . . . f f . . f f . . . . 
+`;
+            case "image3":
+            case "trump-front":return img`
+. . f f f . f f f f f f f . 
+. . f 5 5 f 5 5 4 5 4 5 f f 
+. f 5 5 5 5 5 4 5 4 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 5 f 
+. f 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 4 4 4 4 4 5 5 f . 
+. . f 5 4 4 4 4 4 4 4 f f . 
+. . f 4 b f 4 4 f b 4 f f . 
+. . f 4 1 f 4 4 f 1 4 f f . 
+. . f f 4 4 4 4 4 4 f f . . 
+. . e f e 4 4 4 4 e f e . . 
+. e 4 f 8 1 2 2 1 8 f 4 e . 
+. 4 d f 8 8 5 5 8 8 c d 4 . 
+. 4 4 f 8 8 5 5 8 8 f 4 4 . 
+. . . . f f f f f f . . . . 
+. . . . f f . . f f . . . . 
+`;
+            case "image4":
+            case "elon-front":return img`
+. . . . . . . . . . . . . . 
+. . f . f . . . . . . . . . 
+. f f f f f f f f f f f . . 
+. f f f f f f f f f f f f . 
+. . f f 4 4 4 4 4 4 f f f . 
+. . f 4 d d d d d d 4 f f . 
+. . f d d d d d d d d f . . 
+. . f d b f d d f b d f . . 
+. . f d 1 f d d f 1 d f . . 
+. . . f d d d d d d f f . . 
+. . e f f f d d f f f e . . 
+. e 4 f f f 1 1 2 2 2 4 e . 
+. 4 d f f f 1 1 f 2 f d 4 . 
+. 4 4 f f f 1 1 f 2 f 4 4 . 
+. . . . f f f f f f . . . . 
+. . . . f f . . f f . . . . 
+`;
+            case "image5":
+            case "ice":return img`
+...ffffffffff...
+..ffffffffffff..
+.ff111f11f111ff.
+.fff1ff1ff111ff.
+.fff1ff1ff1ffff.
+.ff111f11f111ff.
+.ffffffffffffff.
+..fddd1111dddf..
+..fbdbfddfbdbf..
+..fcd1f11f1dcf..
+.ffff111111bff..
+fcff1cdb1bdfff..
+f1bffcbfbfff11cf
+fbfbfbfffffb1b1f
+....ffffffffbfbf
+.....ffffff.....
+..fffffffffffff.
+.fffbcfffffcbfff
+.fbc111f.f111cbf
+.fffffff.fffffff
 `;
         }
         return null;
@@ -239,6 +319,59 @@ f f f c 3 c c c c c c f . .
 . . . f e e f 6 6 6 f . . . 
 . . . . f f f f f f . . . . 
 . . . . . f f f . . . . . . 
+`];
+            case "elon-bullet":
+            case "anim5":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f . . . . . . f . . . 
+. . . f . f f . . . . f f . . . 
+. . . f f . f f . . f f . . . . 
+. . . . f f . f f f f . . . . . 
+. . . . . f f . f f . . . . . . 
+. . . . . . f f . f f . . . . . 
+. . . . . . f f f . f f . . . . 
+. . . . . f f . f f . f f . . . 
+. . . . f f . . . f f . f . . . 
+. . . f f . . . . . f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . f f f f . f f f f f . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f . . . . . f f f . . . 
+. . . . f f . . . f f . f . . . 
+. . . . . f f . f f . f f . . . 
+. . . . . . f f f . f f . . . . 
+. . . . . . f f . f f . . . . . 
+. . . . . f f . f f . . . . . . 
+. . . . f f . f f f f . . . . . 
+. . . f f . f f . . f f . . . . 
+. . . f . f f . . . . f f . . . 
+. . . f f f . . . . . . f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 `];
         }
         return null;
--- main.ts
+++ main.ts
@@ -24,85 +24,16 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingRight)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingRight))) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 3 3 3 3 . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . . 3 3 3 . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, nena, 100, 0)
+        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 100, 0)
     } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingDown)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingDown))) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 3 3 3 3 . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . . 3 3 3 . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, nena, 0, 100)
+        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, 100)
     } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingLeft)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingLeft))) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 3 3 3 3 . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . . 3 3 3 . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, nena, -100, 0)
+        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, -100, 0)
     } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingUp)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingUp))) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 . 3 3 3 . . . . . 
-            . . . . 3 3 3 3 3 3 3 . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . 3 3 3 3 3 . . . . . . 
-            . . . . . . 3 3 3 . . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, nena, 0, -100)
+        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, -100)
     } else {
     	
     }
-    pause(2000)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -117,3 +48,9 @@ let nena: Sprite = null
 nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
 nena.setStayInScreen(true)
 controller.moveSprite(nena)
+let statusbar = statusbars.create(20, 2, StatusBarKind.Health)
+statusbar.attachToSprite(nena)
+let enemigo = sprites.create(assets.image`trump-front`, SpriteKind.Enemy)
+game.onUpdate(function () {
+	
+})
