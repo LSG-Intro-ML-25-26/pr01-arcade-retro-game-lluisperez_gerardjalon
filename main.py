90867e822f0f41c26705f9f5053cfc971c520af0
Gerard Jalón <gerard.jalon@gmail.com>
2026-01-20 20:51:58 +0100

    feat: add level1, enemies...

--- main.py
+++ main.py
@@ -1,87 +1,90 @@
-def on_up_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
-controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+def animar_abajo():
+    animation.run_image_animation(nena, assets.animation("""
+        nena-animation-down
+    """), 500, False)
+controller.down.on_event(ControllerButtonEvent.PRESSED, animar_abajo)
 
-def on_a_pressed():
-    global projectile
+def animar_arriba():
+    animation.run_image_animation(nena, assets.animation("""
+        nena-animation-up
+    """), 500, False)
+controller.up.on_event(ControllerButtonEvent.PRESSED, animar_arriba)
+
+def animar_izquierda():
+    animation.run_image_animation(nena, assets.animation("""
+        nena-animation-left
+    """), 500, False)
+controller.left.on_event(ControllerButtonEvent.PRESSED, animar_izquierda)
+
+def animar_derecha():
+    animation.run_image_animation(nena, assets.animation("""
+        nena-animation-right
+    """), 500, False)
+controller.right.on_event(ControllerButtonEvent.PRESSED, animar_derecha)
+
+
+def disparar():
     if characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_RIGHT)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_RIGHT)):
-        projectile = sprites.create_projectile_from_sprite(assets.image("""
-            bullet-1
-            """), nena, 100, 0)
+        sprites.create_projectile_from_sprite(assets.image("bullet-1"), nena, 150, 0)
     elif characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_DOWN)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_DOWN)):
-        projectile = sprites.create_projectile_from_sprite(assets.image("""
-            bullet-1
-            """), nena, 0, 100)
+        sprites.create_projectile_from_sprite(assets.image("bullet-1"), nena, 0, 150)
     elif characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_LEFT)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_LEFT)):
-        projectile = sprites.create_projectile_from_sprite(assets.image("""
-            bullet-1
-            """), nena, -100, 0)
+        sprites.create_projectile_from_sprite(assets.image("bullet-1"), nena, -150, 0)
     elif characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_UP)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_UP)):
-        projectile = sprites.create_projectile_from_sprite(assets.image("""
-            bullet-1
-            """), nena, 0, -100)
-    else:
-        pass
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+        sprites.create_projectile_from_sprite(assets.image("bullet-1"), nena, 0, -150)
 
-def on_left_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-left
-            """),
-        500,
-        False)
-controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+controller.A.on_event(ControllerButtonEvent.PRESSED, disparar)
 
-def on_on_zero(status):
-    sprites.destroy(enemigo)
-statusbars.on_zero(StatusBarKind.enemy_health, on_on_zero)
 
-def on_right_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-right
-            """),
-        500,
-        False)
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+def al_golpear_enemigo(bala, enemigo):
+    sprites.destroy(bala)
+    vida_enemigo = statusbars.get_status_bar_attached_to(StatusBarKind.enemy_health, enemigo)
+    if vida_enemigo:
+        vida_enemigo.value -= 10
 
-def on_down_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-down
-            """),
-        500,
-        False)
-controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, al_golpear_enemigo)
 
-def on_on_overlap(sprite, otherSprite):
-    statusbar2.value -= 10
-    pause(500)
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
 
-projectile: Sprite = None
-enemigo: Sprite = None
+def enemigo_muere(vida):
+    enemigo = vida.sprite_attached_to()
+    if enemigo:
+        sprites.destroy(enemigo)
+
+statusbars.on_zero(StatusBarKind.enemy_health, enemigo_muere)
+
+def iniciar_nivel_1():
+    global nena, vida_jugador
+
+    tiles.set_current_tilemap(tilemap("""
+        nivel1
+    """))
+
+    nena = sprites.create(assets.image("nena-front"), SpriteKind.player)
+    nena.set_position(40, 470)
+    controller.move_sprite(nena)
+    scene.camera_follow_sprite(nena)
+
+    vida_jugador = statusbars.create(20, 2, StatusBarKind.health)
+    vida_jugador.attach_to_sprite(nena)
+
+
+def crear_enemigo_tiktok():
+    enemigo = sprites.create(assets.image("tiktok"), SpriteKind.enemy)
+    enemigo.set_position(nena.x + randint(-60, 60), nena.y + randint(-60, 60))
+
+    vida_enemigo = statusbars.create(20, 2, StatusBarKind.enemy_health)
+    vida_enemigo.max = 20
+    vida_enemigo.attach_to_sprite(enemigo)
+
+    enemigo.follow(nena, 20)
+
+
+def spawn_enemigos():
+    crear_enemigo_tiktok()
+
+
+vida_jugador: StatusBarSprite = None
 nena: Sprite = None
-statusbar2: StatusBarSprite = None
-nena = sprites.create(assets.image("""
-    nena-front
-    """), SpriteKind.player)
-nena.set_stay_in_screen(True)
-controller.move_sprite(nena)
-statusbar = statusbars.create(20, 2, StatusBarKind.health)
-statusbar.attach_to_sprite(nena)
-enemigo = sprites.create(assets.image("""
-    trump-front
-    """), SpriteKind.enemy)
-statusbar2 = statusbars.create(20, 4, StatusBarKind.enemy_health)
-statusbar2.attach_to_sprite(enemigo)
 
-def on_on_update():
-    pass
-game.on_update(on_on_update)
+iniciar_nivel_1()
+game.on_update_interval(4000, spawn_enemigos)
--- main.ts
+++ main.ts
@@ -24,20 +24,37 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingRight)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingRight))) {
-        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 100, 0)
+        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 150, 0)
     } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingDown)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingDown))) {
-        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, 100)
+        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, 150)
     } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingLeft)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingLeft))) {
-        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, -100, 0)
+        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, -150, 0)
     } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingUp)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingUp))) {
-        projectile = sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, -100)
-    } else {
-    	
+        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, -150)
     }
 })
-statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
-    sprites.destroy(enemigo)
+statusbars.onZero(StatusBarKind.EnemyHealth, function (vida) {
+    enemigo = vida.spriteAttachedTo()
+    if (enemigo) {
+        sprites.destroy(enemigo)
+    }
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (bala, enemigo) {
+    sprites.destroy(bala)
+    vida_enemigo = statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, enemigo)
+    if (vida_enemigo) {
+        vida_enemigo.value -= 10
+    }
+})
+function iniciar_nivel_1 () {
+    tiles.setCurrentTilemap(tilemap`nivel1`)
+    nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+    nena.setPosition(40, 470)
+    controller.moveSprite(nena)
+    scene.cameraFollowSprite(nena)
+    vida_jugador = statusbars.create(20, 2, StatusBarKind.Health)
+    vida_jugador.attachToSprite(nena)
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
@@ -46,24 +63,21 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    statusbar2.value -= 10
-pause(500)
-})
-let projectile: Sprite = null
+function crear_enemigo_tiktok () {
+    enemigo2 = sprites.create(assets.image`tiktok`, SpriteKind.Enemy)
+    enemigo2.setPosition(nena.x + randint(-60, 60), nena.y + randint(-60, 60))
+    vida_enemigo2 = statusbars.create(20, 2, StatusBarKind.EnemyHealth)
+    vida_enemigo2.max = 20
+    vida_enemigo2.attachToSprite(enemigo2)
+    enemigo2.follow(nena, 20)
+}
+let vida_enemigo2: StatusBarSprite = null
+let enemigo2: Sprite = null
+let vida_jugador: StatusBarSprite = null
+let vida_enemigo: StatusBarSprite = null
 let enemigo: Sprite = null
-let nena: Sprite = null
-let statusbar2 : StatusBarSprite = null
-nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
-nena.setStayInScreen(true)
-controller.moveSprite(nena)
-let statusbar = statusbars.create(20, 2, StatusBarKind.Health)
-statusbar.attachToSprite(nena)
-enemigo = sprites.create(assets.image`trump-front`, SpriteKind.Enemy)
-statusbar2 = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
-statusbar2.attachToSprite(enemigo)
-scene.setBackgroundColor(0)
-game.onUpdate(function () {
-    let mySprite: Sprite = null
-    mySprite.sayText("test_push")
+let nena : Sprite = null
+iniciar_nivel_1()
+game.onUpdateInterval(4000, function () {
+    crear_enemigo_tiktok()
 })
