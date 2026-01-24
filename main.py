8228f0d26ccca15058d752b9d611479e00f7f1ba
Gerard Jalón <gerard.jalon@gmail.com>
2026-01-24 23:48:50 +0100

    feat: muchos cambios

--- main.py
+++ main.py
@@ -1,85 +1,142 @@
+@namespace
+class SpriteKind:
+    menuItem = SpriteKind.create()
+    cursor = SpriteKind.create()
+    vacio = SpriteKind.create()
+    Rose = SpriteKind.create()
+def sceneStart():
+    global Play
+    Play = sprites.create(assets.image("""
+        parpadeo
+        """), SpriteKind.vacio)
+    while not (controller.A.is_pressed()):
+        Play.set_flag(SpriteFlag.INVISIBLE, True)
+        pause(200)
+        Play.set_flag(SpriteFlag.INVISIBLE, False)
+        pause(200)
+    Play.set_flag(SpriteFlag.INVISIBLE, True)
+    sceneOne()
+
 def on_down_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-down
-            """),
-        500,
-        False)
+    global ultima_direccion
+    if nena:
+        ultima_direccion = "down"
+        animation.run_image_animation(nena,
+            assets.animation("""
+                nena-animation-down
+                """),
+            500,
+            False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 def on_right_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-right
-            """),
-        500,
-        False)
+    global ultima_direccion
+    if nena:
+        ultima_direccion = "right"
+        animation.run_image_animation(nena,
+            assets.animation("""
+                nena-animation-right
+                """),
+            500,
+            False)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
 def on_left_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-left
-            """),
-        500,
-        False)
+    global ultima_direccion
+    if nena:
+        ultima_direccion = "left"
+        animation.run_image_animation(nena,
+            assets.animation("""
+                nena-animation-left
+                """),
+            500,
+            False)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
 def on_a_pressed():
-    if characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_RIGHT)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_RIGHT)):
+    if not (nena):
+        return
+    if ultima_direccion == "right":
         sprites.create_projectile_from_sprite(assets.image("""
             bullet-1
             """), nena, 150, 0)
-    elif characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_DOWN)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_DOWN)):
-        sprites.create_projectile_from_sprite(assets.image("""
-            bullet-1
-            """), nena, 0, 150)
-    elif characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_LEFT)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_LEFT)):
+    elif ultima_direccion == "left":
         sprites.create_projectile_from_sprite(assets.image("""
             bullet-1
             """), nena, -150, 0)
-    elif characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.FACING_UP)) or characterAnimations.matches_rule(nena, characterAnimations.rule(Predicate.MOVING_UP)):
+    elif ultima_direccion == "up":
         sprites.create_projectile_from_sprite(assets.image("""
             bullet-1
             """), nena, 0, -150)
+    elif ultima_direccion == "down":
+        sprites.create_projectile_from_sprite(assets.image("""
+            bullet-1
+            """), nena, 0, 150)
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
-def on_on_zero(vida):
-    global enemigo3
-    enemigo3 = vida.sprite_attached_to()
-    if enemigo3:
-        sprites.destroy(enemigo3)
-statusbars.on_zero(StatusBarKind.enemy_health, on_on_zero)
+def scenethree():
+    scene.set_background_image(assets.image("""
+        fondo_3am1
+        """))
+    game.show_long_text("La pantalla s'estira. El soroll canvia. El temps s'atura. Notes el cos pesat. Com si caiguessis… cap endins.",
+        DialogLayout.BOTTOM)
+    iniciar_nivel_1()
+def abrir_puerta():
+    tiles.set_tile_at(tiles.get_tile_location(10, 5),
+        assets.tile("""
+            transparency16
+            """))
+def scenetwo():
+    scene.set_background_image(assets.image("""
+        fondo_3am0
+        """))
+    game.show_long_text("El mòbil vibra. No és una notificació. Intentes bloquejar-lo. El botó no respon. Només un més. Aquesta vegada no ho decideixes tu.",
+        DialogLayout.BOTTOM)
+    scenethree()
 
-def on_on_overlap(bala, enemigo):
-    global vida_enemigo
-    sprites.destroy(bala)
-    vida_enemigo = statusbars.get_status_bar_attached_to(StatusBarKind.enemy_health, enemigo)
-    if vida_enemigo:
-        vida_enemigo.value -= 10
-sprites.on_overlap(SpriteKind.projectile, SpriteKind.enemy, on_on_overlap)
+def on_on_overlap(player2, rose):
+    global rosas
+    sprites.destroy(rose)
+    rosas += 1
+    if rosas >= 3:
+        abrir_puerta()
+sprites.on_overlap(SpriteKind.player, SpriteKind.Rose, on_on_overlap)
 
 def iniciar_nivel_1():
-    global nena, vida_jugador
+    global nivel, nena, rosa, vida_jugador
+    nivel = 1
     tiles.set_current_tilemap(tilemap("""
         nivel1
         """))
+    for index in range(3):
+        r = sprites.create(assets.image("""
+            rose
+            """), SpriteKind.Rose)
+        tiles.place_on_random_tile(r, assets.tile("""
+            miMosaico
+            """))
     nena = sprites.create(assets.image("""
         nena-front
         """), SpriteKind.player)
     nena.set_position(40, 470)
-    controller.move_sprite(nena)
+    controller.move_sprite(nena, 100, 100)
     scene.camera_follow_sprite(nena)
+    rosa = sprites.create(assets.image("""
+        rose
+        """), SpriteKind.Rose)
     vida_jugador = statusbars.create(20, 2, StatusBarKind.health)
     vida_jugador.attach_to_sprite(nena)
 
 def on_up_pressed():
-    animation.run_image_animation(nena,
-        assets.animation("""
-            nena-animation-up
-            """),
-        500,
-        False)
+    global ultima_direccion
+    if nena:
+        ultima_direccion = "up"
+        animation.run_image_animation(nena,
+            assets.animation("""
+                nena-animation-up
+                """),
+            500,
+            False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
 def crear_enemigo_tiktok():
@@ -92,14 +149,31 @@ def crear_enemigo_tiktok():
     vida_enemigo2.max = 20
     vida_enemigo2.attach_to_sprite(enemigo2)
     enemigo2.follow(nena, 20)
+def sceneOne():
+    scene.set_background_image(assets.image("""
+        fondo_3am
+        """))
+    game.show_long_text("Son les 3:33 AM. El mòbil vibra una altra vegada. No recordes quan has obert TikTok… però tampoc quan l'has deixat.",
+        DialogLayout.BOTTOM)
+    scenetwo()
 vida_enemigo2: StatusBarSprite = None
 enemigo2: Sprite = None
 vida_jugador: StatusBarSprite = None
-vida_enemigo: StatusBarSprite = None
-enemigo3: Sprite = None
+rosa: Sprite = None
+nivel = 0
+rosas = 0
+Play: Sprite = None
+ultima_direccion = ""
 nena: Sprite = None
-iniciar_nivel_1()
+vida_enemigo = None
+enemigo3 = None
+ultima_direccion = "down"
+scene.set_background_image(assets.image("""
+    fondo_inicio0
+    """))
+sceneStart()
 
 def on_update_interval():
-    crear_enemigo_tiktok()
+    if nena and nivel == 1:
+        crear_enemigo_tiktok()
 game.on_update_interval(4000, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,22 +1,55 @@
 namespace SpriteKind {
     export const menuItem = SpriteKind.create()
     export const cursor = SpriteKind.create()
+    export const vacio = SpriteKind.create()
+    export const Rose = SpriteKind.create()
 }
-sprites.onOverlap(SpriteKind.cursor, SpriteKind.menuItem, function (cursor, item) {
-    cursor.sayText("A per començar")
-    if (controller.A.isPressed()) {
-        item.setImage(assets.image`corazon-rojo`)
-        pause(1000)
-        sprites.destroy(cursor)
-        sprites.destroy(item)
-        iniciar_nivel_1()
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Rose, function (player2, rose) {
+    sprites.destroy(rose)
+    rosas += 1
+    if (rosas >= 3) {
+        abrir_puerta()
     }
 })
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+function sceneStart () {
+    Play = sprites.create(assets.image`fondo_inicio0`, SpriteKind.vacio)
+    while (!(controller.A.isPressed())) {
+        Play.setFlag(SpriteFlag.Invisible, true)
+        pause(200)
+        Play.setFlag(SpriteFlag.Invisible, false)
+        pause(200)
+    }
+    Play.setFlag(SpriteFlag.Invisible, true)
+    sceneOne()
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (nena) {
+        ultima_direccion = "down"
         animation.runImageAnimation(
         nena,
-        assets.animation`nena-animation-up`,
+        assets.animation`nena-animation-down`,
+        500,
+        false
+        )
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (nena) {
+        ultima_direccion = "right"
+        animation.runImageAnimation(
+        nena,
+        assets.animation`nena-animation-right`,
+        500,
+        false
+        )
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (nena) {
+        ultima_direccion = "left"
+        animation.runImageAnimation(
+        nena,
+        assets.animation`nena-animation-left`,
         500,
         false
         )
@@ -26,45 +59,65 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (!(nena)) {
         return
     }
-    if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingRight)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingRight))) {
-        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 150, 0)
-    } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingDown)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingDown))) {
-        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, 150)
-    } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingLeft)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingLeft))) {
-        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, -150, 0)
-    } else if (characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.FacingUp)) || characterAnimations.matchesRule(nena, characterAnimations.rule(Predicate.MovingUp))) {
-        sprites.createProjectileFromSprite(assets.image`bullet-1`, nena, 0, -150)
+    if (ultima_direccion == "right") {
+        sprites.createProjectileFromSprite(assets.image`
+            bullet-1
+            `, nena, 150, 0)
+    } else if (ultima_direccion == "left") {
+        sprites.createProjectileFromSprite(assets.image`
+            bullet-1
+            `, nena, -150, 0)
+    } else if (ultima_direccion == "up") {
+        sprites.createProjectileFromSprite(assets.image`
+            bullet-1
+            `, nena, 0, -150)
+    } else if (ultima_direccion == "down") {
+        sprites.createProjectileFromSprite(assets.image`
+            bullet-1
+            `, nena, 0, 150)
     }
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (nena) {
-        animation.runImageAnimation(
-        nena,
-        assets.animation`nena-animation-left`,
-        500,
-        false
-        )
-    }
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (nena) {
-        animation.runImageAnimation(
-        nena,
-        assets.animation`nena-animation-right`,
-        500,
-        false
-        )
-    }
-})
+function scenethree () {
+    scene.setBackgroundImage(assets.image`fondo_3am1`)
+    game.showLongText("La pantalla s'estira. El soroll canvia. El temps s'atura. Notes el cos pesat. Com si caiguessis… cap endins.", DialogLayout.Bottom)
+    iniciar_nivel_1()
+}
+function abrir_puerta () {
+    tiles.setTileAt(tiles.getTileLocation(10, 5), assets.tile`transparency16`)
+}
+function scenetwo () {
+    scene.setBackgroundImage(assets.image`fondo_3am0`)
+    game.showLongText("El mòbil vibra. No és una notificació. Intentes bloquejar-lo. El botó no respon. Només un més. Aquesta vegada no ho decideixes tu.", DialogLayout.Bottom)
+    scenethree()
+}
 function iniciar_nivel_1 () {
+    let r: Sprite;
+nivel = 1
     tiles.setCurrentTilemap(tilemap`nivel1`)
+    for (let index = 0; index < 3; index++) {
+        r = sprites.create(assets.image`rose`, SpriteKind.Rose)
+        tiles.placeOnRandomTile(r, assets.tile`miMosaico`)
+    }
     nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
     nena.setPosition(40, 470)
     controller.moveSprite(nena, 100, 100)
     scene.cameraFollowSprite(nena)
+    rosa = sprites.create(assets.image`rose`, SpriteKind.Rose)
     vida_jugador = statusbars.create(20, 2, StatusBarKind.Health)
     vida_jugador.attachToSprite(nena)
+    rosa_hud = sprites.create(assets.image`hud_rosas`, SpriteKind.Player)
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (nena) {
+        ultima_direccion = "up"
+        animation.runImageAnimation(
+        nena,
+        assets.animation`nena-animation-up`,
+        500,
+        false
+        )
+    }
+})
 function crear_enemigo_tiktok () {
     enemigo2 = sprites.create(assets.image`tiktok`, SpriteKind.Enemy)
     enemigo2.setPosition(nena.x + randint(-60, 60), nena.y + randint(-60, 60))
@@ -73,43 +126,28 @@ function crear_enemigo_tiktok () {
     vida_enemigo2.attachToSprite(enemigo2)
     enemigo2.follow(nena, 20)
 }
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (nena) {
-        animation.runImageAnimation(
-        nena,
-        assets.animation`nena-animation-down`,
-        500,
-        false
-        )
-    }
-})
 function sceneOne () {
-    scene.setBackgroundImage(assets.image`fondoMovil`)
-    game.showLongText("Son les 3:33AM i demà tens examen d'Android, portes mirant TIKTOK dos hores", DialogLayout.Center)
-    game.showLongText("Per donar-li like a un més no passarà res...", DialogLayout.Center)
-    corazon = sprites.create(assets.image`corazon blanco`, SpriteKind.menuItem)
-    corazon.setScale(1.5, ScaleAnchor.BottomLeft)
-    corazon.setPosition(111, 90)
-    corazon.vx = 0
-    corazon.vy = 0
-    cursor2 = sprites.create(assets.image`raton`, SpriteKind.cursor)
-    cursor2.setPosition(154, 56)
-    cursor2.z = 100
-    cursor2.setFlag(SpriteFlag.StayInScreen, true)
-    controller.moveSprite(cursor2, 100, 100)
-    scene.cameraFollowSprite(cursor2)
+    scene.setBackgroundImage(assets.image`fondo_3am`)
+    game.showLongText("Son les 3:33 AM. El mòbil vibra una altra vegada. No recordes quan has obert TikTok… però tampoc quan l'has deixat.", DialogLayout.Bottom)
+    scenetwo()
 }
-let cursor2: Sprite = null
-let corazon: Sprite = null
 let vida_enemigo2: StatusBarSprite = null
 let enemigo2: Sprite = null
+let rosa_hud: Sprite = null
 let vida_jugador: StatusBarSprite = null
+let rosa: Sprite = null
+let nivel = 0
+let Play: Sprite = null
+let rosas = 0
+let ultima_direccion = ""
 let enemigo3 = null
 let vida_enemigo = null
-let nena: Sprite = null
-sceneOne()
+let nena : Sprite = null
+ultima_direccion = "down"
+scene.setBackgroundImage(assets.image`fondo_inicio1`)
+sceneStart()
 game.onUpdateInterval(4000, function () {
-    if (nena) {
+    if (nena && nivel == 1) {
         crear_enemigo_tiktok()
     }
 })
